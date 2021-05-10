bffe9c223dffa95a45f5be431fe751ae46688e5e
Franko Stipanov <fstipanov@gmail.com>
2021-05-10 09:38:03 +0200

    get quote

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+// https://api.quotable.io/random
 'use strict'
+
+const newQuote = document.querySelector('.quote');
+console.log(newQuote);
+
+const getQuote = async () => {
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    return data;
+}
+
+getQuote().then(data => {
+    console.log(data.content);
+    console.log(data.author);
+})
